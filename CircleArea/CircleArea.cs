61cd3285a3c901d5aa6536661462b0191ad12846
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2026-07-01 11:35:41 -0300

    Add project files.

---
+++ CircleArea/CircleArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace Exercises
+{
+    internal class CircleArea
+    {
+        static void Main(string[] args)
+        {
+            double A, R, pi;
+
+            pi = 3.14159;
+            R = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            A = pi * R * R;
+
+            Console.WriteLine("A=" + A.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ ConsoleApp1/AreaCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Exercises
+{
+    internal class AreaCalculator
+    {
+        static void Main(string[] args)
+        {
+            double width, lenght, squareMeterPrice, area, price;
+
+            Console.WriteLine("Enter the width of your area.");
+            width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Now, enter the lenght of your area.");
+            lenght = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Finally, enter the price per square meter.");
+            squareMeterPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = width * lenght;
+            price = squareMeterPrice * area;
+
+            Console.WriteLine("Your area is = " + area.ToString("F2", CultureInfo.InvariantCulture) + " and the price will be = " + price.ToString("F2", CultureInfo.InvariantCulture) + ".");
+        }
+    }
+}
