6d476e2592744fb7fddb05f92b25691f7e20b961
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2026-07-01 18:24:05 -0300

    Updating exercises

---
+++ SimpleProduct/SimpleProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Transactions;
+
+namespace Exercise
+{
+    internal class SimpleProduct 
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, PROD;
+
+            Console.WriteLine("Welcome to the multiplier.");
+
+            Console.WriteLine("Enter a number:");
+            n1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("One more:");
+            n2 = int.Parse(Console.ReadLine());
+
+            PROD = n1 * n2;
+
+            Console.WriteLine("The multiplication of these two numbers is = " + PROD);
+        }
+    }
+}
