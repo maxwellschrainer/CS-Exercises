f0a408de5fa8210ec65ba2a89cd8c2fab22d93f9
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2026-07-03 12:30:11 -0300

    Updating the repository

---
+++ Consumption/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercises
+{
+    internal class Consumption
+    {
+        static void Main(string[] args)
+        {
+            int X = int.Parse(Console.ReadLine());
+            float y = float.Parse(Console.ReadLine());
+
+
+        }
+    }
+}
