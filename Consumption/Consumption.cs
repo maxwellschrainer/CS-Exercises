8d56e9ad9a2f3227ad0c2d3ab5bfdc305d5857ba
Maxwell Schrainer <maxwellschrainer0@gmail.com>
2026-07-04 19:20:52 -0300

    Km consumption

---
+++ Consumption/Consumption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercises
+{
+    internal class Consumption
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's calculate the km consumption your car made");
+
+            Console.WriteLine("");
+
+            Console.WriteLine("First, how much km have you done in the last travel?");
+            int X = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("How much fuel was spent in total?");
+            float y = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            float kml = X / y;
+
+            Console.WriteLine("Your car have done: " + kml.ToString("F3", CultureInfo.InvariantCulture) + " km/l");
+        }
+    }
+}
