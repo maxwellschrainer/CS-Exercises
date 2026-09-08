74f2c6154bebfcfcb15230ebeb8d2257ea8b0853
Max <maxwellschrainer0@gmail.com>
2026-09-08 09:06:12 -0300

    Updating

--- Distance/Distance.cs
+++ Distance/Distance.cs
@@ -5,8 +5,12 @@ namespace Exercises
     internal class Distance
     {
         static void Main(string[] args)
-        { 
-        
+        {
+            int distance = int.Parse(Console.ReadLine());
+            int tempo;
+            tempo = distance * 2;
+
+            Console.WriteLine(tempo + " minutos");
         }
     }
 }
