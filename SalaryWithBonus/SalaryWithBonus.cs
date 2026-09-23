3cab471f36917cf934740b7c378dafee290396a5
Max <maxwellschrainer0@gmail.com>
2026-09-23 18:21:18 -0300

    Another exercise done

---
+++ SalaryWithBonus/SalaryWithBonus.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace exercises 
+{ 
+    internal class SalaryWithBonus
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double salary, salesvalue, salestotal, total;
+
+            name = Console.ReadLine();
+            salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salesvalue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) * 0.15;
+            salestotal = salesvalue ;
+            total = salestotal + salary;
+
+            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
