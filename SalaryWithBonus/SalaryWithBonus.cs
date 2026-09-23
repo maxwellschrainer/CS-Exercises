f8a2abd65d21a3e479bee78ac8fb683bcc20d5f4
Max <maxwellschrainer0@gmail.com>
2026-09-23 18:28:42 -0300

    Adding more strings

--- SalaryWithBonus/SalaryWithBonus.cs
+++ SalaryWithBonus/SalaryWithBonus.cs
@@ -8,15 +8,20 @@ namespace exercises
         static void Main(string[] args)
         {
             string name;
-            double salary, salesvalue, salestotal, total;
+            double salary, salesvalue, total;
 
+            Console.WriteLine("Type your name:");
             name = Console.ReadLine();
-            salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            salesvalue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) * 0.15;
-            salestotal = salesvalue ;
-            total = salestotal + salary;
 
-            Console.WriteLine("TOTAL = R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Type your salary:");
+            salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Type the value from all your sales:");
+            salesvalue = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture) * 0.15;
+
+            total = salesvalue + salary;
+
+            Console.WriteLine(name + ", doing the sum from the 15% of your sales, plus your salary, you will receive: $ " + total.ToString("F2", CultureInfo.InvariantCulture) + " this month.");
             Console.ReadLine();
         }
     }
