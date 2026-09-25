5ce1202dccf58fb095ad54934316b350de8515a6
Max <maxwellschrainer0@gmail.com>
2026-09-25 08:52:41 -0300

    Creating a calculation of a sphere

--- Sphere/Sphere.cs
+++ Sphere/Sphere.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Exercises
 {
@@ -6,7 +7,18 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
+            double pi = 3.14159;
 
+            Console.WriteLine("Lets calculate the volume of a sphere.");
+
+            Console.WriteLine("Type the radius of the sphere: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double volume = (4 / 3.0) * pi * Math.Pow(radius, 3);
+
+            Console.WriteLine("The volume of the sphere is: " + volume.ToString("F3", CultureInfo.InvariantCulture));
+
+            Console.ReadLine();
         }
     }
 }
